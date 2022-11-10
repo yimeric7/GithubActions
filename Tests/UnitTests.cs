9727ed91a3cc9dae58252eec6ac9c73961dc1d2b
yimeric7 <112520475+yimeric7@users.noreply.github.com>
2022-11-09 22:17:00 -0600

    fixed unit tests

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -108,7 +108,7 @@ namespace GithubActionsLab
         {
             Assert.AreEqual(1, Program.Power("1", "2"));
             Assert.AreEqual(9, Program.Power("3", "2"));
-            Assert.AreEqual(25, Program.Divide("5", "2"));
+            Assert.AreEqual(25, Program.Power("5", "2"));
         }
 
         [Test]
